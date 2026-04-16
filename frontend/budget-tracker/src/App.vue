e4bf0e9c21dd9066caaba94d7861c7c82b0b12bd
CDStLouis <c.st.louis@outlook.com>
2026-04-16 14:35:26 +0100

    inital commit

---
+++ frontend/budget-tracker/src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+
+fetch("https://localhost:5156/weatherforecast")
+  .then(res => res.json())
+  .then(data => console.log(data))
+</script>
+
+<template>
+  <HelloWorld />
+</template>
